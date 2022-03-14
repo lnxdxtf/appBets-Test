e1ea34ba0cc765dddd655e98c7b59078cdeacca6
lnxdxtf <gabrielramosmichaliszen@gmail.com>
2022-03-13 21:33:04 -0300

    ok

--- appBets/config.js
+++ appBets/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 module.exports = {
     mongoURI : process.env.MONGO_STRING,
-    PORT : process.env.PORT || 3000,
+    PORT : process.env.PORT || 3000 ||8000 || 9000 || 8080,
 }
